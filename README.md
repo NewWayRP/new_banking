aba095d5f3f07060119ed474c4867ff9d7507837
Nico-Home <me@nicolas-masse.fr>
2018-08-26 05:56:31 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Based on onlyserenity script (https://github.com/onlyserenity/new_banking)
+New UI and some improvments
+
+---
+
+FXServer NEW_BANKING
+
+[REQUIREMENTS]
+
+Dependencies For Full Functionality
+
+es_extended => https://github.com/ESX-Org/es_extended
+
+Install To resources/[esx]/new_banking << MUST BE INSTALLED HERE
+
+Add this in your server.cfg :
+start new_banking
+
+Credits: Script Created By: @onlyserenity(amjedcha) edited and improved by NewWay
+
+Discord: https://discord.gg/dSYb4j2
+
+---
+
+![](https://i.imgur.com/bjfLLr9.png)
+![](https://i.imgur.com/JnlPAHN.png)
+![](https://i.imgur.com/OU5B0yo.png)
+![](https://i.imgur.com/t9axH2n.png)
+![](https://i.imgur.com/MH7MdKA.png)
---
+++ server.lua
@@ -0,0 +1,78 @@
+--================================================================================================
+--==                                VARIABLES - DO NOT EDIT                                     ==
+--================================================================================================
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent('bank:deposit')
+AddEventHandler('bank:deposit', function(amount)
+	local _source = source
+	
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	if amount == nil or amount <= 0 or amount > xPlayer.getMoney() then
+		TriggerClientEvent('bank:result', _source, "error", "Montant invalide.")
+	else
+		xPlayer.removeMoney(amount)
+		xPlayer.addAccountMoney('bank', tonumber(amount))
+		TriggerClientEvent('bank:result', _source, "success", "Dépot effectué.")
+	end
+end)
+
+
+RegisterServerEvent('bank:withdraw')
+AddEventHandler('bank:withdraw', function(amount)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local base = 0
+	amount = tonumber(amount)
+	base = xPlayer.getAccount('bank').money
+	if amount == nil or amount <= 0 or amount > base then
+		TriggerClientEvent('bank:result', _source, "error", "Montant invalide.")
+	else
+		xPlayer.removeAccountMoney('bank', amount)
+		xPlayer.addMoney(amount)
+		TriggerClientEvent('bank:result', _source, "success", "Retrait effectué.")
+	end
+end)
+
+RegisterServerEvent('bank:balance')
+AddEventHandler('bank:balance', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	balance = xPlayer.getAccount('bank').money
+	TriggerClientEvent('currentbalance1', _source, balance)
+end)
+
+
+RegisterServerEvent('bank:transfer')
+AddEventHandler('bank:transfer', function(to, amountt)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local zPlayer = ESX.GetPlayerFromId(to)
+	local balance = 0
+
+	if(zPlayer == nil or zPlayer == -1) then
+		TriggerClientEvent('bank:result', _source, "error", "Destinataire introuvable.")
+	else
+		balance = xPlayer.getAccount('bank').money
+		zbalance = zPlayer.getAccount('bank').money
+		
+		if tonumber(_source) == tonumber(to) then
+			TriggerClientEvent('bank:result', _source, "error", "Vous ne pouvez pas faire de transfert à vous même.")
+		else
+			if balance <= 0 or balance < tonumber(amountt) or tonumber(amountt) <= 0 then
+				TriggerClientEvent('bank:result', _source, "error", "Vous n'avez pas assez d'argent en banque.")
+			else
+				xPlayer.removeAccountMoney('bank', tonumber(amountt))
+				zPlayer.addAccountMoney('bank', tonumber(amountt))
+				TriggerClientEvent('bank:result', _source, "success", "Transfert effectué.")
+			end
+		end
+	end
+end)
+
+
+
+
+
